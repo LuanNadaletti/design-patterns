20b8f4a166cf69220658225c734554f1939ea602
LuanNadaletti <luannadaletti6@gmail.com>
2023-09-12 16:12:44 -0300

    :recycle: separada a configuração da parte visual em pequenas funções.

--- src/main/java/com/patterns/strategy/StrategyPattern.java
+++ src/main/java/com/patterns/strategy/StrategyPattern.java
@@ -19,9 +19,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
+import com.patterns.strategy.contexts.SortContext;
 import com.patterns.strategy.strategies.BubbleSort;
-import com.patterns.strategy.strategies.InsertionSort;
-import com.patterns.strategy.strategies.SortStrategy;
 
 /**
  * The <code>StrategyPattern</code> class is the main entry point to demonstrate
@@ -56,6 +55,7 @@ class SortingFrame extends JFrame {
     private final JComboBox<String> algorithmComboBox;
     private final JTextArea inputTextArea;
     private final JTextArea outputTextArea;
+    private SortContext sortContext;
 
     /**
      * Constructs a new SortingFrame with all the necessary GUI components.
@@ -63,64 +63,22 @@ class SortingFrame extends JFrame {
     public SortingFrame() {
         setTitle("Sorting App");
         setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        sortContext = new SortContext();
 
         algorithmComboBox = new JComboBox<>();
-        algorithmComboBox.addItem("Bubble Sort");
-        algorithmComboBox.addItem("Insertion Sort");
-        algorithmComboBox.setSelectedItem("Bubble Sort");
+        initializeAlgorithmComboBox();
 
-        inputTextArea = new JTextArea(10, 20);
-        outputTextArea = new JTextArea(10, 20);
-        outputTextArea.setEditable(false);
-        outputTextArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        inputTextArea = createTextArea(10, 20);
+        outputTextArea = createOutputTextArea();
 
-        inputTextArea.setLineWrap(true);
-        outputTextArea.setLineWrap(true);
+        JButton sortButton = createSortButton();
+        JButton randomListButton = createRandomListButton();
 
-        JButton sortButton = new JButton("Sort");
-        sortButton.addActionListener(e -> {
-            String selectedAlgorithm = (String) algorithmComboBox
-                    .getSelectedItem();
-            SortStrategy sortStrategy = getSortStrategyByLabel(
-                    selectedAlgorithm);
-
-            int[] array = inputArrayUsingDialog();
-            if (array != null) {
-                sortStrategy.sort(array);
-                outputTextArea.setText(sortStrategy.getSortInfo());
-            }
-        });
-
-        JButton randomListButton = new JButton("Random Numbers");
-        randomListButton.addActionListener(e -> {
-            StringBuilder randomNumbersListToString = new StringBuilder();
-            for (Integer number : generateRandomList()) {
-                randomNumbersListToString.append(number + " ");
-            }
-            inputTextArea.setText(randomNumbersListToString.toString());
-        });
-
-        JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
-        inputPanel.add(new JLabel("Enter numbers separated by spaces:"));
-        inputPanel.add(new JScrollPane(inputTextArea));
-
-        JPanel outputPanel = new JPanel();
-        outputPanel.setLayout(new BoxLayout(outputPanel, BoxLayout.Y_AXIS));
-        outputPanel.add(new JLabel("Sort Information:"));
-        outputPanel.add(new JScrollPane(outputTextArea));
-
-        JPanel algorithmPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        algorithmPanel.add(new JLabel("Choose sorting algorithm:"));
-        algorithmPanel.add(algorithmComboBox);
-
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.add(sortButton);
-        buttonPanel.add(randomListButton);
-
-        JPanel inputOutputPanel = new JPanel(new GridLayout(1, 2, 10, 0));
-        inputOutputPanel.add(inputPanel);
-        inputOutputPanel.add(outputPanel);
+        JPanel inputOutputPanel = createInputOutputPanel();
+        JPanel algorithmPanel = createAlgorithmPanel();
+        JPanel buttonPanel = createButtonPanel(sortButton, randomListButton);
 
         setLayout(new BorderLayout());
         add(algorithmPanel, BorderLayout.NORTH);
@@ -160,21 +118,146 @@ class SortingFrame extends JFrame {
     }
 
     /**
-     * Returns the sort strategy based on the label provided.
-     *
-     * @param label the name/label of the sorting algorithm.
-     * @return a {@link SortStrategy} instance based on the given label, or null
-     *         if not matched.
+     * Initializes the algorithm selection combo box with the available sorting
+     * algorithm options.
      */
-    private SortStrategy getSortStrategyByLabel(String label) {
-        if (label.equalsIgnoreCase("bubble sort")) {
-            return new BubbleSort();
-        }
-        if (label.equalsIgnoreCase("insertion sort")) {
-            return new InsertionSort();
-        }
+    private void initializeAlgorithmComboBox() {
+        algorithmComboBox.addItem("Bubble Sort");
+        algorithmComboBox.addItem("Insertion Sort");
+        algorithmComboBox.addItem("Selection Sort");
+        algorithmComboBox.setSelectedItem(BubbleSort.class);
+    }
 
-        return null;
+    /**
+     * Creates and configures a JTextArea for user input with the specified
+     * number of rows and columns.
+     *
+     * @param rows    The number of rows in the text area.
+     * @param columns The number of columns in the text area.
+     * @return The configured JTextArea for user input.
+     */
+    private JTextArea createTextArea(int rows, int columns) {
+        JTextArea textArea = new JTextArea(rows, columns);
+        textArea.setLineWrap(true);
+        return textArea;
+    }
+
+    /**
+     * Creates and configures a JTextArea for displaying output with a fixed
+     * font and size.
+     *
+     * @return The configured JTextArea for displaying output.
+     */
+    private JTextArea createOutputTextArea() {
+        JTextArea textArea = createTextArea(10, 20);
+        textArea.setEditable(false);
+        textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+        return textArea;
+    }
+
+    /**
+     * Creates a JButton for initiating the sorting process and adds an
+     * ActionListener to it.
+     *
+     * @return The configured JButton for sorting.
+     */
+    private JButton createSortButton() {
+        JButton sortButton = new JButton("Sort");
+        sortButton.addActionListener(e -> {
+            String selectedSort = (String) algorithmComboBox.getSelectedItem();
+
+            int[] array = inputArrayUsingDialog();
+            if (array != null) {
+                sortContext.setSortStrategy(selectedSort);
+                sortContext.sort(array);
+                outputTextArea
+                        .setText(sortContext.getSortStrategy().getSortInfo());
+            }
+        });
+        return sortButton;
+    }
+
+    /**
+     * Creates a JButton for generating random numbers and adds an
+     * ActionListener to it.
+     *
+     * @return The configured JButton for generating random numbers.
+     */
+    private JButton createRandomListButton() {
+        JButton randomListButton = new JButton("Random Numbers");
+        randomListButton.addActionListener(e -> {
+            StringBuilder randomNumbersListToString = new StringBuilder();
+            for (Integer number : generateRandomList()) {
+                randomNumbersListToString.append(number).append(" ");
+            }
+            inputTextArea.setText(randomNumbersListToString.toString());
+        });
+        return randomListButton;
+    }
+
+    /**
+     * Creates a JPanel with a GridLayout containing input and output panels.
+     *
+     * @return The JPanel containing input and output panels.
+     */
+    private JPanel createInputOutputPanel() {
+        JPanel panel = new JPanel(new GridLayout(1, 2, 10, 0));
+        panel.add(createInputPanel());
+        panel.add(createOutputPanel());
+        return panel;
+    }
+
+    /**
+     * Creates a JPanel for user input with a vertical BoxLayout.
+     *
+     * @return The JPanel for user input.
+     */
+    private JPanel createInputPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("Enter numbers separated by spaces:"));
+        panel.add(new JScrollPane(inputTextArea));
+        return panel;
+    }
+
+    /**
+     * Creates a JPanel for displaying sorting output with a vertical BoxLayout.
+     *
+     * @return The JPanel for displaying sorting output.
+     */
+    private JPanel createOutputPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("Sort Information:"));
+        panel.add(new JScrollPane(outputTextArea));
+        return panel;
+    }
+
+    /**
+     * Creates a JPanel for selecting sorting algorithms with a left-aligned
+     * FlowLayout.
+     *
+     * @return The JPanel for selecting sorting algorithms.
+     */
+    private JPanel createAlgorithmPanel() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        panel.add(new JLabel("Choose sorting algorithm:"));
+        panel.add(algorithmComboBox);
+        return panel;
+    }
+
+    /**
+     * Creates a JPanel for grouping and aligning buttons horizontally.
+     *
+     * @param buttons The buttons to be added to the panel.
+     * @return The JPanel containing the specified buttons.
+     */
+    private JPanel createButtonPanel(JButton... buttons) {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        for (JButton button : buttons) {
+            panel.add(button);
+        }
+        return panel;
     }
 
     /**
