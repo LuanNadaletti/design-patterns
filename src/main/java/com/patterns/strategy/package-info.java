bacd0674f1ecaeaa5f075752efa27a582b7286f3
LuanNadaletti <luannadaletti6@gmail.com>
2023-09-19 09:51:04 -0300

    :memo: adicionada documentação do package de Strategy.

---
+++ src/main/java/com/patterns/strategy/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package contains an example of the Strategy design pattern
+ * implementation. The Strategy pattern defines a family of algorithms,
+ * encapsulates each one, and makes them interchangeable. It allows clients to
+ * choose the appropriate algorithm at runtime.
+ *
+ * <p>
+ * In this example, sorting algorithms are implemented using the Strategy
+ * pattern. The application provides a Swing GUI that allows users to select an
+ * algorithm, input numbers, and perform sorting operations while displaying
+ * information about the sorting process.
+ * <p>
+ *
+ * @author Luan Nadaletti
+ */
+package com.patterns.strategy;
